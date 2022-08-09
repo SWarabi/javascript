c58b385a2230a9b6960289703a9f70088ae81992
EC2 Default User <ec2-user@ip-172-31-20-112.ap-northeast-1.compute.internal>
2022-08-09 15:04:20 +0000

    finish

---
+++ app.js
@@ -0,0 +1,16 @@
+// let alertString;
+// alertString = addString("WebCamp");
+
+// // 作成した関数を呼び出す
+// alert(alertString);
+
+// // 作成した関数
+// function addString(strA){
+//   let addStr = "Hello " + strA;
+//   return addStr;
+// }
+// let promptStr = prompt('何か好きな文字を入力してください。');
+
+// alert(promptStr);
+
+// じゃんけんの手を入力してもらうプロンプト欄を生成
